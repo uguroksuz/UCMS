204d709ac0a28cdb87f1f1b3317325d0e1010b2c
uguroksuz <uguroksuz@outlook.com>
2018-11-22 00:53:00 +0300

    sortable eklenerek dinamik sırlana sağlandı.

--- panel/assets/assets/js/custom.js
+++ panel/assets/assets/js/custom.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    $(".sortable").sortable();
+
     $(".remove-btn").click(function(e){
 
         var $data_url = $(this).data("url");
@@ -29,7 +31,14 @@ $(document).ready(function(){
 
     })
 
+    $(".sortable").on("sortupdate", function(event, ui){
+        var $data = $(this).sortable("serialize");
+        var $data_url = $(this).data("url");
 
+        $.post($data_url, {data : $data }, function(response){})
+
+
+    })
 
 })
 		
