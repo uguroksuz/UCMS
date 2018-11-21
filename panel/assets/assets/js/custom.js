4d8dfacae43678a0c2efaeb266cd7a69460a67d6
uguroksuz <uguroksuz@outlook.com>
2018-11-21 23:18:02 +0300

    sweetalert eklendi

---
+++ panel/assets/assets/js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    swal({
+        title: 'Error!',
+        text: 'Do you want to continue',
+        type: 'error',
+        confirmButtonText: 'Cool'
+    })
+})
+		
