731e86f7b2ad7b367a0f53dbe1d1516404662f4a
uguroksuz <uguroksuz@outlook.com>
2018-11-21 21:24:38 +0300

    library.js dosaysı library.php dosyasına taşındı. Base url kullanabilmek için. Ürün eklemek için gerekli olan view tasarımı gerçekleştirildi.

---
+++ panel/application/views/product_v/add/content.php
@@ -0,0 +1,27 @@
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">
+            Yeni Ürün Ekle
+        </h4>
+
+    </div><!-- END column -->
+    <div class="col-md-12">
+        <div class="widget">
+            <hr class="widget-separator">
+            <div class="widget-body">
+                <form action="<?php echo base_url("product/save"); ?>" method="post">
+                    <div class="form-group">
+                        <label>Başlık</label>
+                        <input class="form-control" id="exampleInputEmail1" placeholder="Başlık" name="title">
+                    </div>
+                    <div class="form-group">
+                        <label>Açıklama</label>
+                        <textarea name="description" class="m-0" data-plugin="summernote" data-options="{height: 250}"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-outline btn-primary btn-md">Kaydet</button>
+                    <a href="<?php echo base_url("product"); ?>" class="btn btn-outline btn-danger btn-md">İptal</a>
+                </form>
+            </div><!-- .widget-body -->
+        </div><!-- .widget -->
+    </div><!-- END column -->
+</div>
