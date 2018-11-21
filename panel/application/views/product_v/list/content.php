58e14cb61a4ee80c7a140128b32ed1bd06cb9ffb
uguroksuz <uguroksuz@outlook.com>
2018-11-21 17:50:33 +0300

    ürün eklme için liste sayfasına eklemeler yapıldı.

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,1 +1,42 @@
-ürün listesi content
+<div class="row">
+    <div class="col-md-12">
+        <h4 class="m-b-lg">
+            Ürün Listesi
+            <a href="#" class="btn btn-outline btn-primary pull-right"><i class="fa fa-plus"></i> Yeni Ekle</a>
+        </h4>
+
+    </div><!-- END column -->
+    <div class="col-md-12">
+        <div class="widget p-lg">
+
+            <div class="alert alert-info alert-custom">
+                <p>Herhangi bir veri bulunmamaktadır. Eklemek için lütfen <a href="$">tıklayınız.</a></p>
+            </div>      
+            <table class="table table-hover table-striped">
+                <thead>
+                    <th>#id</th>
+                    <th>url</th>
+                    <th>Başlık</th>
+                    <th>Açıklama</th>
+                    <th>Durumu</th>
+                    <th>İşlem</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>#1</td>
+                        <td>monitor-askisi</td>
+                        <td>Monitör Askısı</td>
+                        <td>360 derece monitör askısı</td>
+                        <td>
+                            <input id="switch-0-3" type="checkbox" data-switchery checked />
+                        </td>
+                        <td>
+                            <a href="#" class="btn btn-xs btn-outline btn-warning"><i class="fa fa-trash"></i> Sil</a>
+                            <a href="#" class="btn btn-xs btn-outline btn-primary"><i class="fa fa-edit"></i> Düzenle</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div><!-- .widget -->
+    </div><!-- END column -->
+</div>
