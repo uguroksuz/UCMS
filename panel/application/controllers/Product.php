4a2e719f8b44336320244d59ba4fd8e14242d7ae
uguroksuz <uguroksuz@outlook.com>
2018-11-21 22:19:39 +0300

    seo uyumlu url elde edilecek helper yazıldı

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -63,7 +63,7 @@ class Product extends CI_Controller
                 array(
                     "title"         => $this->input->post("title"),
                     "description"   => $this->input->post("description"),
-                    "url"           => "test...",
+                    "url"           => convertToSeo($this->input->post("title")),
                     "rank"          => 0,
                     "isActive"      => 1,
                     "createdAt"     => date("Y-m-d H:i:s")
