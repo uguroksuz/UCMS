b7377519bb7f79b6a95ebbe32a9267e82587d187
uguroksuz <uguroksuz@outlook.com>
2018-11-23 17:22:16 +0300

    haber update işlemi yapıldı.

--- panel/application/controllers/News.php
+++ panel/application/controllers/News.php
@@ -200,7 +200,7 @@ class News extends CI_Controller
         $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
     }
 
-    public function update($id){
+    public function update_($id){
         
         $this->load->library("form_validation");
 
@@ -265,6 +265,139 @@ class News extends CI_Controller
             $viewData->item = $item;
 
             
+
+            $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
+        }
+
+    }
+
+    public function update($id){
+        
+        $this->load->library("form_validation");
+
+        //kurallar yazılır..
+
+        $news_type = $this->input->post("news_type");
+        
+        if($news_type == "video"){
+
+            $this->form_validation->set_rules("video_url", "Video URL", "required|trim");
+
+        }
+        
+
+        $this->form_validation->set_rules("title", "Başlık", "required|trim");
+
+        $this->form_validation->set_message(
+            array(
+                "required" => "{field} alanı doldurulmalıdır."
+            )
+        );
+
+        //form validation çalıştırılır.
+        $validate = $this->form_validation->run();
+
+        if ($validate) {
+
+
+            if($news_type == "image"){
+
+                if ($_FILES["img_url"]["name"] != "") {
+                    $file_name = convertToSeo(pathinfo($_FILES["img_url"]["name"], PATHINFO_FILENAME)) . "." . pathinfo($_FILES["img_url"]["name"], PATHINFO_EXTENSION);
+
+                    $config["allowed_types"] = "jpg|jepg|png";
+                    $config["upload_path"] = "uploads/$this->viewFolder/";
+                    $config["file_name"] = $file_name;
+                    
+                    $this->load->library("upload", $config);
+    
+                    $upload = $this->upload->do_upload("img_url");
+    
+                    if ($upload) {
+    
+                        $uploaded_file = $this->upload->data("file_name");
+    
+                        $data =  array(
+                            "title"         => $this->input->post("title"),
+                            "description"   => $this->input->post("description"),
+                            "url"           => convertToSeo($this->input->post("title")),
+                            "news_type"     => $news_type,
+                            "img_url"     => $uploaded_file,
+                            "video_url"     => "#",
+                        );
+    
+                    } else {
+                        $alert = array(
+                            "title"      =>"İşlem Başarısız.",
+                            "message"    =>"Görsel yüklenirken bir problem oluştu.",
+                            "type"      =>"error"
+                        );
+    
+                        $this->session->set_flashdata("alert", $alert);
+    
+                        redirect(base_url("news/update_form/$id"));
+                    }
+    
+                } else {
+                    $data =  array(
+                        "title"         => $this->input->post("title"),
+                        "description"   => $this->input->post("description"),
+                        "url"           => convertToSeo($this->input->post("title")),
+                    );
+                }
+
+            } elseif ($news_type == "video") {
+                $data =  array(
+                    "title"         => $this->input->post("title"),
+                    "description"   => $this->input->post("description"),
+                    "url"           => convertToSeo($this->input->post("title")),
+                    "news_type"     => $news_type,
+                    "img_url"       => "#",
+                    "video_url"     => $this->input->post("video_url"),
+                );
+            }
+            
+            $update = $this->news_model->update(array("id" => $id), $data);
+
+            //TODO: alert sistemi eklenecek.
+            if ($update) {
+
+                $alert = array(
+                    "title"      =>"İşlem Başarılı",
+                    "message"    =>"Güncelleme işlemi başarılı.",
+                    "type"      =>"success"
+                );
+
+            }else {
+
+                $alert = array(
+                    "title"      =>"İşlem Başarısız.",
+                    "message"    =>"Kayıt güncelleme sırasında bir problem oluştu.",
+                    "type"      =>"error"
+                );
+                          
+            }
+            
+            // işlem sonucunu sessiona yazıyoruz.
+            $this->session->set_flashdata("alert", $alert);
+
+            redirect(base_url("news"));
+
+        }else {
+            $viewData = new stdClass();
+
+            $viewData->viewFolder = $this->viewFolder;
+            $viewData->subViewFolder = "update";
+            $viewData->form_error = true;
+            $viewData->news_type = $news_type;
+
+            // Tablodan ilgili veri getiriliyor.
+            $viewData->item = $this->news_model->get(
+                array(
+                    "id"        => $id
+                )
+            );
+
 
             $this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index", $viewData);
         }
